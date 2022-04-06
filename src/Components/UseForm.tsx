f3ae2bd8f327c84d999b49af01c3834454641902
unknown <mohdarif013579@gmail.com>
2022-04-06 12:00:55 +0530

    add use State in typescript

---
+++ src/Components/UseForm.tsx
@@ -0,0 +1,31 @@
+import React ,{useState} from 'react'
+
+interface IUserForm {
+    name:string,
+    age:number
+}
+
+const defaultValue = {
+    name:'',
+    age:0
+}
+
+const UseForm = () => {
+    const [form , setForm] = useState<IUserForm>(defaultValue);
+    
+   const onChangeName = (event :any) => {
+       setForm({
+           ...form,
+           name:event.target.value
+       })
+
+   }
+
+  return (
+    <div>
+        <input onChange={onChangeName}  />
+    </div>
+  )
+}
+
+export default UseForm
