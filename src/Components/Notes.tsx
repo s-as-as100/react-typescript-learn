e2b08879dce249aee9dca91dc78045adfbacf1b9
unknown <mohdarif013579@gmail.com>
2022-04-21 16:21:57 +0530

    Difference b/w type and interface and changeenventHandleer

--- src/Components/Notes.tsx
+++ src/Components/Notes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { ChangeEventHandler } from "react";
 // typescript is static type
 //it's just JavaScript, with static typing.
 
@@ -17,35 +17,72 @@ const Notes = () => {
     b,
     c
   ) => {};
-// which type of data type is return by this function -- > if we say is void -->
-// --> nothing is return
+  // which type of data type is return by this function -- > if we say is void -->
+  // --> nothing is return
 
-// what is interface --> is define which type of data type of key value pair 
+  // what is interface --> is define which type of data type of key value pair
+
+  // one way
+  // interface ISearchProps extends IchangeHandleProps {
+  //     className:string,
+  //     placeholder:string,
+
+  // }
+  // interface IchangeHandleProps {
+  //     onChangeHandle : (a: string) => void
+  // }
+
+  // 2nd way
+  interface ISearchProps {
+    onChangeHandle: (a: string) => void;
+  }
+
+  // one another way is type
+
+  type SearchBoxProps = {
+    className: string;
+    placeholder?: string | null;
+
+    onChangeHandle: ChangeEventHandler<HTMLInputElement>;
+  };
+
+  // difference between type and interface is
+  // Interface should be extends
+  // type --> Combination of type
+  // type --> u can use multiple type union
+
+  // if u are using functional type programming u can used type and if ur component
+  // is class based then u can use interface
 
 
-// one way 
-interface ISearchProps extends IchangeHandleProps {
-    className:string,
-    placeholder:string,
+  type EastDelhi = {
+    streetNumber: string;
+    pincode: number;
+    address: string;
+  };
 
-    
-}
-// interface IchangeHandleProps {
-//     onChangeHandle : (a: string) => void
-// }
+  type WestDelhi = {
+    streetNumber: string;
+    galiNumber: string;
+  };
 
+  type addressEastAndWest = EastDelhi | WestDelhi;
 
-// 2nd way
-interface ISearchProps {
-    onChangeHandle : (a: string) => void 
-}
+  const address: addressEastAndWest = {
+    streetNumber: "heelo",
+    galiNumber: "gali number",
+  };
 
-
-  const SearchComponent = ({ className, placeholder, onChangeHandle } : ISearchProps) => {
+  const SearchComponent = ({
+    className,
+    placeholder,
+    onChangeHandle,
+  }: SearchBoxProps) => {
     return (
       <input
         className={className}
         placeholder={placeholder}
+        // onChange={(e) =>onChangeHandle(e)}
         onChange={onChangeHandle}
       />
     );
